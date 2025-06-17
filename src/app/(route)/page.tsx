6bc85cbafb649eb0e081f5c86159a0b419648e87
winter100 <100taeng@gmail.com>
2025-06-17 15:58:56 +0900

    feat: 전체 공연페이지 구현

--- src/app/(route)/page.tsx
+++ src/app/(route)/page.tsx
@@ -1,3 +1,19 @@
+import ContainerWrapper from '@/components/layout/container-wrapper';
+import { Suspense } from 'react';
+import { ErrorBoundary } from 'react-error-boundary';
+import { Show } from '../_features/home';
+
 export default function Page() {
-  return <div>Home</div>;
+  return (
+    <ContainerWrapper>
+      <h1 className="mr-auto text-4xl font-bold select-none">공연</h1>
+
+      {/* 에러 및 로딩 컴포넌트 변경 예정 */}
+      <ErrorBoundary fallback={<div>...에러</div>}>
+        <Suspense fallback={<div>...로딩</div>}>
+          <Show />
+        </Suspense>
+      </ErrorBoundary>
+    </ContainerWrapper>
+  );
 }
