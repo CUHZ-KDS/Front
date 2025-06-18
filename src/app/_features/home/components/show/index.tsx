6b0280047e4c32a70d85961a7ec56c853969b886
winter100 <100taeng@gmail.com>
2025-06-18 18:26:30 +0900

    refactor: 렌더러 프롭스 방식으로 변경

--- src/app/_features/home/components/show/index.tsx
+++ src/app/_features/home/components/show/index.tsx
@@ -3,6 +3,7 @@ import ShowCarousel from '@/components/showCarousel';
 import ShowDetailDialog from '../dialog';
 import { useShowList } from '../../hooks/useShowList';
 import { useShowDialog } from '../../hooks/useShowDialog';
+import Poster from '@/components/poster';
 
 export default function Show() {
   const { data: showList } = useShowList();
@@ -10,7 +11,11 @@ export default function Show() {
 
   return (
     <div className="w-full">
-      <ShowCarousel showList={showList.data} onItemClick={handleItemClick} />
+      <ShowCarousel
+        showList={showList.data}
+        onItemClick={handleItemClick}
+        renderItem={item => <Poster className="h-[400px]" image={item.img_source} />}
+      />
       {selectedItem && <ShowDetailDialog onClose={handleDialogClose} selectedItem={selectedItem} />}
     </div>
   );
