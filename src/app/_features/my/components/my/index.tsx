e26fe9e2707b9db12899eab65d0df8a820d31a43
winter100 <100taeng@gmail.com>
2025-06-18 16:57:48 +0900

    feat: 예매 캐러셀 UI  및 취소 버튼 구현 (더미)

---
+++ src/app/_features/my/components/my/index.tsx
@@ -0,0 +1,44 @@
+'use client';
+import ShowCarousel from '@/components/showCarousel';
+import { useMyShowList } from '../../hooks/useMy';
+import { Button } from '@/components/ui/button';
+import Poster from '@/components/poster';
+
+export default function My() {
+  const { data: myShowList } = useMyShowList();
+
+  return (
+    <div className="w-full">
+      <ShowCarousel
+        showList={myShowList.data.reservations}
+        isInteractive={false}
+        renderItem={item => (
+          <div className="relative">
+            {/* 추후 리팩토링 필요 */}
+            <Poster className="h-[400px] opacity-20" image={item.show.img_sorce} />
+            <div className="absolute bottom-0 p-6">
+              <h2 className="text-xl">{item.show.title}</h2>
+              <div>
+                <div>row: {item.seat.row}</div>
+                <div>column: {item.seat.column}</div>
+              </div>
+            </div>
+            <div className="absolute inset-0 z-30 rounded-xl bg-white/10"></div>
+          </div>
+        )}
+        renderBottomContent={item => (
+          <div className="py-1">
+            {/* 추후 리팩토링 필요 (취소 핸들러 예정) */}
+            <Button
+              onClick={() => console.log(item.id)}
+              variant="rr"
+              className="ml-auto block w-36 cursor-pointer"
+            >
+              예매 취소하기
+            </Button>
+          </div>
+        )}
+      />
+    </div>
+  );
+}
--- src/components/ui/button.tsx
+++ src/components/ui/button.tsx
@@ -18,6 +18,7 @@ const buttonVariants = cva(
         ghost: 'hover:bg-accent hover:text-accent-foreground dark:hover:bg-accent/50',
         link: 'text-primary underline-offset-4 hover:underline',
         ff: 'rounded-md bg-[#4000FF] text-white hover:bg-[#4000FF]/90',
+        rr: 'rounded-md bg-[#F12B28] text-white hover:bg-[#F12B28]/90',
       },
       size: {
         default: 'h-9 px-4 py-2 has-[>svg]:px-3',
